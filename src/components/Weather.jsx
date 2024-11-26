57fee698c994e711ea1759d75517b34d14f20476
KashaveniSruthi <kashavenisruthi@gmail.com>
2024-11-26 13:31:59 +0530

    first commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,31 +2,46 @@ import React, { useEffect ,useRef,useState} from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
-import drizzle_icon from '../assets/drizzle.png'
-import humidity_icon from '../assets/humidity.png'
-import snow_icon from '../assets/snow.png'
-import wind_icon from '../assets/wind.png'
+import clear2_icon from '../assets/clear2.png'
+import fewclouds_icon from '../assets/fewclouds.png'
+import fewclouds2_icon from '../assets/fewclouds2.png'
+import scatteredclouds_icon from '../assets/scatteredclouds.png'
+import scatteredclouds2_icon from '../assets/scatteredclouds2.png'
+import brokenclouds_icon from '../assets/brokenclouds.png'
+import brokenclouds2_icon from '../assets/brokenclouds2.png'
+import showerrain_icon from '../assets/showerrain.png'
+import showerrain2_icon from '../assets/showerrain2.png'
 import rain_icon from '../assets/rain.png'
+import rain2_icon from '../assets/rain2.png'
+import thunderstrom_icon from '../assets/thunderstrom.png'
+import thunderstrom2_icon from '../assets/thunderstrom2.png'
+import snow_icon from '../assets/snow.png'
+import snow2_icon from '../assets/snow2.png'
+import mist_icon from '../assets/mist.png'
+import humidity_icon from '../assets/humidity.png'
+import wind_icon from '../assets/wind.png'
 
 const Weather = () => {
     const inputRef=useRef()
     const [weatherData, setWeatherData] = useState(false);
     const allIcons={
         "01d":clear_icon,
-        "01n":clear_icon,
-        "02d":cloud_icon,
-        "02n":cloud_icon,
-        "03d":cloud_icon,
-        "03n":drizzle_icon,
-        "04d":drizzle_icon,
-        "04n":rain_icon,
-        "09d":rain_icon,
-        "09n":rain_icon,
+        "01n":clear2_icon,
+        "02d":fewclouds_icon,
+        "02n":fewclouds2_icon,
+        "03d":scatteredclouds_icon,
+        "03n":scatteredclouds2_icon,
+        "04d":brokenclouds_icon,
+        "04n":brokenclouds2_icon,
+        "09d":showerrain_icon,
+        "09n":showerrain2_icon,
         "10d":rain_icon,
-        "10n":rain_icon,
+        "10n":rain2_icon,
         "13d":snow_icon,
-        "13n":snow_icon,
+        "13n":snow2_icon,
+        "50d":mist_icon,
+        "50n":mist_icon,
+
     }
     const search=async(city)=>{
         if(city===""){
